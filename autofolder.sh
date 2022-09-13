8850155b6dc8f1891dac907c11d494a7857e2f14
blfivm <113402702+blfivm@users.noreply.github.com>
2022-09-13 11:12:17 +0800

    Add files via upload

---
+++ autofolder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+mkdir $1
+touch $2
+mv $2 $1
+
+echo "The [$1] Directory has been created and populated with the file [$2]"
+
+exit 0
---
+++ intro.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name=$1
+age=$2
+graduate=$3
+
+echo "My name is $name, my age is $age and i will graduate ECU in $graduate"
+
+exit 0
+
+
+
